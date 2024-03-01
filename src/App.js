6a0c6146a8e0c8dcf0d7a35731724f1bed75e86c
Israel Dyett <israel.dyett@massygroup.com>
2024-03-01 14:22:41 -0400

    fixed the url for the gallary

--- src/App.js
+++ src/App.js
@@ -11,15 +11,16 @@ function App() {
       <div className="App">
         <Profile />
         <Routes> {/* Wrap your Route components in a Routes element */}
-          <Route exact path="/" element={<ProjectGallery />} />
+          <Route exact path="/Portfolio/" element={<ProjectGallery />} />
+          
           {/* Add more routes as needed */}
         </Routes>
         <div className='footertext'>
-        <p className='footertext'>
-          <a href="mailto:israeldyett@gmail.com">israeldyett@gmail.com</a> |
-          <a href="https://www.linkedin.com/in/israel-dyett-1ab385249/">LinkedIn</a> |
-          <a href="https://github.com/IsraelDyett">GitHub</a>
-        </p>
+          <p className='footertext'>
+            <a href="mailto:israeldyett@gmail.com">israeldyett@gmail.com</a> |
+            <a href="https://www.linkedin.com/in/israel-dyett-1ab385249/">LinkedIn</a> |
+            <a href="https://github.com/IsraelDyett">GitHub</a>
+          </p>
         </div>
       </div>
     </Router>
