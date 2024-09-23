a6b6030fdb8e8632fd85bc2ac5c8e1d2a8ba536d
IsraelDyett <140478310+IsraelDyett@users.noreply.github.com>
2024-09-23 14:31:29 -0400

    after massy Update ProjectGallery.js

--- src/components/ProjectGallery.js
+++ src/components/ProjectGallery.js
@@ -1,31 +1,39 @@
 // ProjectGallery.js
 import React from 'react';
 import ProjectCard from './ProjectCard';
-import './ProjectGallery.css'; // Import your CSS file for styling
+import './ProjectGallery.css'; 
 import jqi from "./content/jqi.png"
-import bms from "./content/bms.png"
-import wp from "./content/wp.png"
+import bms from "./content/budget.png"
+import wp from "./content/welcomemassy.png"
 import zoho from "./content/zohoDiagram.png"
 import chatbot from "./content/chatbotdiagram.png"
 import PromoConnect from "./content/promoConnect.png"
-import confidential from "./content/confidential.png"
+import onmarkttCatbot from "./content/OnmarkTTChatbot.png"
+import salesCommission from "./content/SalesCommissins.png"
 
 const ProjectGallery = () => {
   // Define project data
   const projects = [
-    {
-      title: "Budget Management System",
-      description: "Crafted using C# ASP.NET Core, our groundbreaking project revolutionizes the management of monthly budget reports at Massy Group. This sophisticated solution boasts a comprehensive suite of features, including dynamic CRUD operations on SQL databases, innovative object-oriented business logic for user, access, and budget management, along with meticulous action log tracking for auditing purposes. Developed on the .NET Entity Framework, our application adheres to the MVC design pattern, ensuring scalability, efficiency, and an unparalleled user experience.",
-      url: "https://example.com/project1",
-      link: "https://github.com/IsraelDyett/BudgetManager",
-      image: confidential
-    },
     {
       title: "Promoconnet: A Comprehensive SaaS Platform Showcasing Full-Stack Development Mastery",
       description: "Promoconnet is a sophisticated SaaS application I developed using the MERN stack, showcasing my expertise in full-stack development and the MVC design pattern. The platform enables users to post images and videos, build comprehensive profiles and portfolios, and engage with company-posted events by liking, commenting, and applying. Company users can efficiently manage event applications, accepting or rejecting them as needed. This project demonstrates my expertise in developing interactive, user-centric web applications, integrating multiple functionalities, and leveraging technologies like React, Node.js, Express, and MongoDB. My experience with Promoconnet highlights my ability to create scalable, feature-rich applications that meet diverse user needs and business objectives While this project is nearing a close, I still find it necessary to continuously refine and optimize Promoconnet to ensure it meets the highest standards of performance and user experience. This ongoing process involves integrating user feedback, enhancing existing features, and exploring new technologies to keep the application relevant and competitive. My commitment to iterative development and continuous improvement underscores my dedication to delivering top-quality software solutions that align with evolving user needs and industry trends.",
       url: "https://promo-connect-client.vercel.app/",
       link: "https://github.com/IsraelDyett/PromoConnect",
       image: PromoConnect
+    },
+    {
+      title: "AI-Powered Facebook Messenger Chatbot with Dialogflow and OpenAI Integration",
+      description: "Developed an AI-driven chatbot that integrates Facebook Messenger with Dialogflow and OpenAI to provide automated, intelligent responses to customer inquiries. The system retrieves customer messages from Facebook Messenger and passes them to Dialogflow for intent detection. Using Firebase, it pulls past conversation data for contextual understanding, which is then leveraged by OpenAI to generate accurate, context-aware responses. These responses are sent back to the customer and stored in Firebase for future interactions. The entire system is connected via APIs, and the webhook endpoints are hosted on Google Cloud using Node.js, ensuring scalable and secure communication between platforms.",
+      url: "https://github.com/IsraelDyett/OnMarkTT-Chatbot",
+      link: "https://github.com/IsraelDyett/OnMarkTT-Chatbot",
+      image: onmarkttCatbot
+    },
+    {
+      title: "Full-Stack Web Application for Bulk Data Management",
+      description: "Developed a full-stack web application that enables seamless CRUD operations, along with bulk editing, deletion, and CSV upload capabilities. The frontend was built using SvelteKit and Tailwind CSS, ensuring a responsive and dynamic user interface. Integrated a .NET C# ASP API on the backend with Windows Authentication to ensure secure access. The system efficiently handles large datasets, supporting bulk data management for improved operational productivity.",
+      url: "https://github.com/IsraelDyett/SalesCommissionAPI-Massy",
+      link: "https://github.com/IsraelDyett/SalesCommision_Interface_client-Massy",
+      image: salesCommission
     },
     {
       title: "Zoho Inventory Management Automation",
@@ -37,9 +45,16 @@ const ProjectGallery = () => {
     {
       title: "GooglePlatform Dialogflow Custom Integration",
       description: "I designed and deployed a sophisticated AI chatbot leveraging Dialogflow, accessible across WhatsApp, Facebook, email, and websites. By integrating seamlessly with CRM, inventory, and database systems via APIs and webhooks, I ensured efficient data flow and automation. Utilizing Google Cloud Platform and Node.js, I achieved scalable and robust performance, enhancing user engagement through intelligent automation and real-time data synchronization. This project exemplifies my ability to deliver integrated solutions that optimize business processes and drive impactful user interactions.",
-      url: "https://github.com/IsraelDyett/GooglePlatform-Dialogflow-Custom-Integration",
-      link: "https://github.com/IsraelDyett/GooglePlatform-Dialogflow-Custom-Integration",
+      url: "https://github.com/IsraelDyett/BudgetManager-Massy",
+      link: "https://github.com/IsraelDyett/BudgetManager-Massy",
       image: chatbot
+    }, 
+    {
+      title: "Budget Management System",
+      description: "Crafted using C# ASP.NET Core, our groundbreaking project revolutionizes the management of monthly budget reports at Massy Group. This sophisticated solution boasts a comprehensive suite of features, including dynamic CRUD operations on SQL databases, innovative object-oriented business logic for user, access, and budget management, along with meticulous action log tracking for auditing purposes. Developed on the .NET Entity Framework, our application adheres to the MVC design pattern, ensuring scalability, efficiency, and an unparalleled user experience.",
+      url: "https://example.com/project1",
+      link: "https://github.com/IsraelDyett/BudgetManager",
+      image: bms
     },
     {
       title: "Email Ordering Solution",
@@ -53,7 +68,7 @@ const ProjectGallery = () => {
       description: "Employing HTML and CSS, our frontend development endeavor delivers a captivating welcome page coupled with intelligent routing capabilities tailored to users' locations. Through meticulous styling and design, we've crafted an immersive welcome experience that sets the stage for seamless navigation. Leveraging HTML's structural prowess and CSS's aesthetic finesse, our welcome page exudes elegance and sophistication, enticing users from the moment they arrive. Our routing mechanism ingeniously guides users based on their geographical location, ensuring personalized interactions and relevant content delivery. Whether users hail from bustling metropolises or serene countrysides, our frontend seamlessly adapts, providing an intuitive and tailored browsing experience. This fusion of HTML and CSS prowess not only elevates aesthetics but also enhances functionality, setting new standards for frontend development excellence.",
       url: "https://www.massystores.com/",
       link: "https://github.com/IsraelDyett/massystoresWelcomePage",
-      image: confidential
+      image: wp
     },
     
   ];
